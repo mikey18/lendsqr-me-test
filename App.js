2ae4d7d99d252516dd9579021bc30d2904fad671
fastestFingers <oluwatimilehinseriki@gmail.com>
2024-11-04 01:32:01 +0100

    update

--- App.js
+++ App.js
@@ -78,8 +78,8 @@ const App = () => {
                     }}
                 >
                     <Section title="Step One">
-                        Edit <Text style={styles.highlight}>App.tsx</Text> to
-                        change this screen and then come back to see your edits.
+                        Edit <Text style={styles.highlight}>App.js</Text> to
+                        change this screen and then come back to see your edit.
                     </Section>
                     <Section title="See Your Changes">
                         <ReloadInstructions />
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
-api.cache(false)
+// api.cache(false)
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv']
-  ]
+  // plugins: [
+  //   ['module:react-native-dotenv']
+  // ]
 };
